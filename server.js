4e4952a770647fc10e466bb3a57e9b02e27b318e
Gayathri Ganesan <Gayathri Ganesan>
2017-02-28 20:29:44 +0530

    Modified code

--- server.js
+++ server.js
@@ -5,16 +5,114 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+ "article-one": {
+   'heading':'Article One',
+    'title': 'Save Nature',
+    'content':`<pre>
+    Forgiving the sinner is God's nature
+Giving food to eat is trees' and plants' nature
+Bringing happiness in one's minds is birds' and butterflies' nature
+Showing the path of life is light's nature
+Clearing human's thirst is water's nature
+Tolerating the burden of human is Earth’s nature
+Increasing lifespan of human is air's nature
+Shrinking the nature is human nature
+If the nature of human be so, then
+How long will nature be a heaven?
+Though nature is bestowed with powers
+They do not misuse human but showers
+All vital needs of human being
+Just for people's well being.
+Save nature for a bright future
+Else, human life will shrink through disaster.﻿
+               - Gayathri Ganesan
+             </pre>`
+  },
+  'article-two': {
+    'heading':'Article Two',
+     'title': 'BeYouTiful',
+     'content':`<pre>
+     Being alone in your room
+And mirror there in front of you,
+By the unique poses you give,
+Your reflection shows the real YOU.
+As if no one notices,
+You would behave in the way you used to be.
+By the stolen shots of a camera,
+The pictures show the real YOU.
+Be as you wish to be,
+And stay different from others.
+By the uniqueness one possess,
+Beautiful it always looks,the real YOU :-)
+                  - Gayathri Ganesan
+               </pre>
+              `
+   }
+}
+function changeContent(page){
+        var template=`<!doctype html>
+        <html>
+            <head>
+                <link href="/ui/style.css" rel="stylesheet" />
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                 <title> ${page.heading} </title>
+            </head>
+            <body>
+            <div class="content ">
+                <div class="center text-big bold">
+                    ${page.title}
+                </div>
+                <br>
+                <div class="center">
+                ${page.content}
+                </div>
+                <hr/>
+                <h4> Add comment </h4>
+                <div>
+                <label>Name: <input type="text" name="name" id="name"></label>
+                </div>
+                <br/>
+                <div>
+                <label>Comment:</label> <textarea rows="4" cols="50"  id="comment" autocomplete="off"></textarea>
+                </div>
+                <br/>
+                <input type="submit" id="submit-button" value="Submit"/>
+                <hr/>
+                <h4> Comments </h4>
+                <div id="comments">
+                </div>
+                  </div>
+                <script type="text/javascript" src="/ui/main.js">
+                </script>
+            </body>
+        </html>
+        `
+  return template;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/:articleName',function(req,res){
+   articleName=req.params.articleName;
+   console.log(articleName+"is this");
+   console.log("hariii"+articles[articleName]);
+   res.send(changeContent(articles[articleName]));
+})
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/indian.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'indian.jpg'));
+});
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
